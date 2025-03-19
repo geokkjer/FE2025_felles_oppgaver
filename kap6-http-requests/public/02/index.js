9e2616e64a48632bbb043ac42a1cc9387bb9081e
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-03-19 11:22:02 +0100

    x

--- kap6-http-requests/public/02/index.js
+++ kap6-http-requests/public/02/index.js
@@ -1,4 +1,5 @@
 import todos from './todos.js';
+import fs from 'fs';
 
 const printResult = (action, result) => {
   const time = new Date().toTimeString();
@@ -8,6 +9,18 @@ const printResult = (action, result) => {
   )} (${time})`;
 
   document.querySelector('div').appendChild(node);
+
+  fs.readFile('/test.json', (error, data) => {
+    if (error) {
+      console.error(error);
+
+      throw err;
+    }
+
+    const user = JSON.parse(data);
+
+    console.log(user);
+  });
 };
 
 const onListClick = async () => {
@@ -43,7 +56,7 @@ const onDeleteClick = async () => {
 
 const onDeleteAllClick = async () => {
   const list = await todos.list();
-  let result = '';
+  let result = [];
   for (let i = 0; i < list.length; i++) {
     const { id } = list[i];
     result = await todos.delete(id);
@@ -51,8 +64,26 @@ const onDeleteAllClick = async () => {
   printResult('delete all todo', result);
 };
 
-const onUpdateAllCompleted = async () => {};
+const onUpdateAllCompleted = async () => {
+  const list = await todos.list();
+  let result = '';
+  for (let i = 0; i < list.length; i++) {
+    const { id } = list[i];
+    const newTodo = {
+      id,
+      completed: true,
+    };
 
+    result = await todos.update(newTodo);
+  }
+  result = await todos.list();
+
+  printResult('update all to completed todo', result);
+};
+
+document
+  .querySelector('button[data-update-all]')
+  .addEventListener('click', onUpdateAllCompleted);
 document
   .querySelector('button[data-delete-all]')
   .addEventListener('click', onDeleteAllClick);
