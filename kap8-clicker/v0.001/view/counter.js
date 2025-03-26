ab8e46c628817019f268a1f3183de2b900495a09
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-03-26 13:32:11 +0100

    kap8-clicker moved folders

--- kap8-clicker/v0.001/view/counter.js
+++
@@ -1,9 +0,0 @@
-const getScoreCount = score => { 
-    return `Current Score is: ${score}`
-  }
-  
-  export default (targetElement, { score }) => {
-    const newCounter = targetElement.cloneNode(true)
-    newCounter.textContent = getScoreCount(score)
-    return newCounter
-  }
