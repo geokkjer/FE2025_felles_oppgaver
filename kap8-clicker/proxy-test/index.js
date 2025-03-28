a33c42c2ec8753ae06511a283591194a65829fc3
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-03-28 14:46:47 +0100

    clicker v2.1

--- kap8-clicker/proxy-test/index.js
+++ kap8-clicker/proxy-test/index.js
@@ -1,20 +1,34 @@
 const base = {
-    foo: 0,
+  score: 0,
+  update: 1,
+};
+
+const handler = {
+  get: (target, name) => {
+    console.log(`Getting ${name}`);
+    return target[name];
+  },
+  set: (target, name, value) => {
+    console.log(`Setting ${name} to ${value}`);
+    target[name] = value;
+    return true;
+  },
+};
+
+const proxy = new Proxy(base, handler);
+document.body.addEventListener(
+  "click",
+  () => (document.getElementById("score").innerHTML = /*HTML*/ `${proxy.score}`)
+);
+document
+  .querySelector("#up")
+  .addEventListener("click", () => (proxy.score = proxy.score + proxy.update));
+document.querySelector("#upgrade").addEventListener("click", () => {
+  if (proxy.score >= 10) {
+    proxy.score -= 10;
+    proxy.update += 1
+  } else {
+    alert('No!!!!')
   }
-  
-  const handler = {
-    get: (target, name) => {
-      console.log(`Getting ${name}`)
-      return target[name]
-    },
-    set: (target, name, value) => {
-      console.log(`Setting ${name} to ${value}`)
-      target[name] = value
-      return true
-    }
-  }
-  
-  const proxy = new Proxy(base, handler)
-  
-  proxy.foo = proxy.foo + 1
-  console.log(`Logging ${proxy.foo}`)
+});
+
