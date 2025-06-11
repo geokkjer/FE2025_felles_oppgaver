aecd7f615d5f75fc196c8420928b29baa42e45ff
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-06-11 09:16:50 +0200

    added envrc and flake

--- Array Utility-pakke × 3/index.js
+++
@@ -1,6 +0,0 @@
-import mockData from "./src/mock-data.js";
-import *from "./src/utils-direct.js";
-
-console.log("Array Utility Package Loaded");
-
-console.log("last in mock data:", mockData.last());
